604433dbc1b62d83e5fb9ea14a9f5c47487f3473
Jesse Decker <me@jessedecker.com>
2015-01-05 10:23:23 -0800

    adding Failure/Redis::limitQueueLength For use in fully-automated systems.

--- lib/Failure/Redis.php
+++ lib/Failure/Redis.php
@@ -14,25 +14,41 @@ use stdClass;
  */
 class Redis implements Failure_Interface
 {
+    const QUEUE_NAME = 'failed';
+
     /**
      * Initialize a failed job class and save it (where appropriate).
      *
      * @param object $payload   Object containing details of the failed job.
-     * @param object $exception Instance of the exception that was thrown by the failed job.
+     * @param \Exception $exception Instance of the exception that was thrown by the failed job.
      * @param object $worker    Instance of Resque\Worker that received the job.
      * @param string $queue     The name of the queue the job was fetched from.
      */
     public function __construct($payload, $exception, $worker, $queue)
     {
-        $data            = new stdClass;
-        $data->failed_at = strftime('%a %b %d %H:%M:%S %Z %Y');
-        $data->payload   = $payload;
-        $data->exception = get_class($exception);
-        $data->error     = $exception->getMessage();
-        $data->backtrace = explode("\n", $exception->getTraceAsString());
-        $data->worker    = (string) $worker;
-        $data->queue     = $queue;
-        $data            = json_encode($data);
-        Resque::redis()->rpush('failed', $data);
+        $data = array(
+            'failed_at' => strftime('%a %b %d %H:%M:%S %Z %Y'),
+            'payload'   => $payload,
+            'exception' => get_class($exception),
+            'error'     => $exception->getMessage(),
+            'backtrace' => explode("\n", $exception->getTraceAsString()),
+            'worker'    => (string) $worker,
+            'queue'     => $queue,
+        );
+        $data = json_encode($data);
+        Resque::redis()->rpush(self::QUEUE_NAME, $data);
+    }
+
+    /**
+     * Delete any failure queue items after $len length.
+     *
+     * @param $len int
+     */
+    public static function limitQueueLength($len)
+    {
+        $redis = Resque::redis();
+        while ($redis->llen(self::QUEUE_NAME) > $len) {
+            $redis->lpop(self::QUEUE_NAME);
+        }
     }
 }
